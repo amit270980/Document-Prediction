bdaeb2af16f705f781ad4e312185fe3faa6adac5
Amit kumar <49388813+amit270980@users.noreply.github.com>
2024-03-09 15:46:53 +1100

    folder updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='US visa',
+    version= '0.0.0',
+    author= 'Amit Kumar',
+    author_email= 'amit270980@gmail.com',
+    packages= find_packages()  
+)
